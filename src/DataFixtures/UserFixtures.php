cd6a00885ead9cc37c18210cee8cf5dd84d210fb
moreno-guillaume <moreno-guillaume@outlook.fr>
2026-01-09 16:13:48 +0100

    feat: fixtrues utilisateurs (admin + user)

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher
+    ) {}
+
+    public function load(ObjectManager $manager): void
+    {
+        // User admin
+        $admin = new User();
+        $admin->setEmail('admin@pokedex.fr');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'admin'));
+        $manager->persist($admin);
+
+        // User classique
+        $user = new User();
+        $user->setEmail('user@pokedex.fr');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'user'));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
