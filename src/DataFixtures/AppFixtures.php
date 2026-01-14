efe6e71673f706aa58451bc38670c4600e0cd65a
moreno-guillaume <moreno-guillaume@outlook.fr>
2026-01-14 13:58:56 +0100

    feat: ajout fixtures avec 20 pokemon

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Pokemon;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,34 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $pokemons = [
+            'Bulbizarre',
+            'Herbizarre',
+            'Florizarre',
+            'Salamèche',
+            'Reptincel',
+            'Dracaufeu',
+            'Carapuce',
+            'Carabaffe',
+            'Tortank',
+            'Pikachu',
+            'Raichu',
+            'Rondoudou',
+            'Psykokwak',
+            'Magicarpe',
+            'Léviator',
+            'Évoli',
+            'Aquali',
+            'Voltali',
+            'Pyroli',
+            'Mew'
+        ];
+
+        foreach ($pokemons as $name) {
+            $pokemon = new Pokemon();
+            $pokemon->setLabel($name);
+            $manager->persist($pokemon);
+        }
 
         $manager->flush();
     }
